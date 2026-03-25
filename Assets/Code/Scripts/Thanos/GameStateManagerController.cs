724b9d6072d85c3821587f9746cd79899208bff5
Christina Kaliora <christina.kaliora@pirron.one>
2026-03-25 11:48:07 +0200

    Added old OnGUI buttons to make the state changing test easier.

--- Assets/Code/Scripts/Thanos/GameStateManagerController.cs
+++ Assets/Code/Scripts/Thanos/GameStateManagerController.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 
 public class GameStateManagerController : MonoBehaviour
 {
-    [SerializeField] private TMPro.TMP_Text gameStateText;
-
     private void Start()
     {
         if (GameStateManager.Instance == null)
@@ -13,11 +11,6 @@ public class GameStateManagerController : MonoBehaviour
         }
 
         GameStateManager.Instance.OnStateChanged += HandleStateChanged;
-
-        if (gameStateText != null)
-        {
-            gameStateText.text = $"State: {GameStateManager.Instance.CurrentState}";
-        }
     }
 
     private void OnDestroy()
@@ -28,24 +21,36 @@ public class GameStateManagerController : MonoBehaviour
         }
     }
 
-    private void Update()
+    private void OnGUI()
     {
         if (GameStateManager.Instance == null) return;
 
-        if (Input.GetKeyDown(KeyCode.Z)) GameStateManager.Instance.RequestStateChange(GameState.Lobby);
-        if (Input.GetKeyDown(KeyCode.X)) GameStateManager.Instance.RequestStateChange(GameState.Loading);
-        if (Input.GetKeyDown(KeyCode.C)) GameStateManager.Instance.RequestStateChange(GameState.InGame);
-        if (Input.GetKeyDown(KeyCode.V)) GameStateManager.Instance.RequestStateChange(GameState.PostGame);
-        if (Input.GetKeyDown(KeyCode.B)) GameStateManager.Instance.RequestStateChange(GameState.Menu);
+        GUILayout.BeginArea(new Rect(10, 10, 200, 300));
+        GUILayout.Label($"Current State: {GameStateManager.Instance.CurrentState}");
+
+        if (GUILayout.Button("Menu")) GameStateManager.Instance.RequestStateChange(GameState.Menu);
+        if (GUILayout.Button("Lobby")) GameStateManager.Instance.RequestStateChange(GameState.Lobby);
+        if (GUILayout.Button("Loading")) GameStateManager.Instance.RequestStateChange(GameState.Loading);
+        if (GUILayout.Button("InGame")) GameStateManager.Instance.RequestStateChange(GameState.InGame);
+        if (GUILayout.Button("PostGame")) GameStateManager.Instance.RequestStateChange(GameState.PostGame);
+
+        GUILayout.EndArea();
     }
+
+    // private void Update()
+    // {
+    //     if (GameStateManager.Instance == null) return;
+
+    //     if (Input.GetKeyDown(KeyCode.Z)) GameStateManager.Instance.RequestStateChange(GameState.Lobby);
+    //     if (Input.GetKeyDown(KeyCode.X)) GameStateManager.Instance.RequestStateChange(GameState.Loading);
+    //     if (Input.GetKeyDown(KeyCode.C)) GameStateManager.Instance.RequestStateChange(GameState.InGame);
+    //     if (Input.GetKeyDown(KeyCode.V)) GameStateManager.Instance.RequestStateChange(GameState.PostGame);
+    //     if (Input.GetKeyDown(KeyCode.B)) GameStateManager.Instance.RequestStateChange(GameState.Menu);
+    // }
 
     public void HandleStateChanged(GameState previous, GameState next)
     {
         Debug.Log($"[GameStateManagerController]: State Changed from {previous} to {next}");
-        if (gameStateText != null)
-        {
-            gameStateText.text = $"State: {next}";
-        }
 
     }
 }
