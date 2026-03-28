992ebd9376be53b7e66f7d7967ecdf17ea97f73c
mikepaneras <mikepaneras21@gmail.com>
2026-03-28 13:58:20 +0200

    -Save System Refactoring and Improvements

--- Assets/Code/Scripts/Dimitris/SaveData.cs
+++ Assets/Code/Scripts/Dimitris/SaveData.cs
@@ -1,5 +1,5 @@
 ///<summary>
-///This is Script that is been used like a data container. For now has only position for testing, later more will be added whatever needs to be saved 
+///This is Script that is been used like a data container.
 ///</summary>
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +7,5 @@ using UnityEngine;
 [System .Serializable]
 public class SaveData
 {
-    public Vector3 Position;
-    public Dictionary<string, string> ExtraData = new Dictionary<string, string>(); //This Dictionary is for later for modular Objects
+    
 }
