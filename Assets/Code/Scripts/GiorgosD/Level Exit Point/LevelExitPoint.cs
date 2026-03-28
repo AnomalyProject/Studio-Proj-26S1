ec572801ac3d651ab9fa9e4574266b9bf22b24c4
GeoD <geo2006@hotmail.gr>
2026-03-28 16:46:21 +0000

    Level exit point
    needs to colliders one trigger and one more for the "button". I have a place holder for how many players in the box that will be replaced with the how many players are in the lobby from networking.

---
+++ Assets/Code/Scripts/GiorgosD/Level Exit Point/LevelExitPoint.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelExitPoint : MonoBehaviour, IInteractable<FPSController>
+{
+    [Header("Settings")]
+    [SerializeField] private LayerMask playerMask;
+    [SerializeField] private Collider interablecCollider;
+
+    // Checks
+    private bool bCanInteract;
+    [SerializeField] private bool bHasAnomaly;
+
+    private HashSet<FPSController> playersInArea = new HashSet<FPSController>();
+
+    // Events
+    public event Action<bool> OnActivateExit;
+
+    public bool CanInteract(FPSController interactor) => bCanInteract;
+
+    public bool TryInteract(FPSController interactor)
+    {
+        if (!bCanInteract)
+        {
+            return false;
+        }
+
+        Debug.Log($"Gay test");
+        Exit();
+        return true;
+    }
+
+    #region Exit
+    /// <summary>
+    /// notifies the game with an event weather there is an anomaly or not.
+    /// </summary>
+    private void Exit()
+    {
+        Debug.Log($"Exit Activated. Anomaly Presence: {bHasAnomaly}");
+        OnActivateExit?.Invoke(bHasAnomaly);
+    }
+    #endregion
+
+    #region Triggers
+    /// <summary>
+    /// Checks if player(uses layer mask for player detection) is in the trigger volume.
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if(((1 << other.gameObject.layer) & playerMask) != 0)
+        {
+            if (other.TryGetComponent(out FPSController player))
+            {
+                playersInArea.Add(player);
+
+                CheckPlayersInArea();
+            }
+        }
+    }
+
+    // See OnTriggerEnter summary.
+    private void OnTriggerExit(Collider other)
+    {
+        if (((1 << other.gameObject.layer) & playerMask) != 0)
+        {
+            if (other.TryGetComponent(out FPSController player))
+            {
+                playersInArea.Remove(player);
+
+                CheckPlayersInArea();
+            }
+        }
+    }
+    #endregion
+
+    #region Player in Area Count
+    /// <summary>
+    /// Checks if all players are in the area.
+    /// </summary>
+    private void CheckPlayersInArea()
+    {
+        int totalPlayers = 1; // Test data, will be replaced with actual player count from the Networking
+
+        bCanInteract = playersInArea.Count >= totalPlayers;
+
+        Debug.Log($"Players in Area: {playersInArea.Count}/{totalPlayers}. Can Interact: {bCanInteract}");
+    }
+    #endregion
+
+    #region Interactable Collider
+    /// <summary>
+    /// Enables/Desables the collider of the interactable.
+    /// </summary>
+    /// <param name="state"></param>
+    public void SetCollider(bool state)
+    {
+        if (interablecCollider != null)
+        {
+            interablecCollider.enabled = state;
+        }
+    }
+    #endregion
+}
