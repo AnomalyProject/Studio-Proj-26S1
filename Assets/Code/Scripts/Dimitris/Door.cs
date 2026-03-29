dd6b4e0704f7d8606c0700eeed9894694cd9af03
dataktos <dataktos@gmail.com>
2026-03-29 13:13:36 +0300

    door-open-close-interactable
    A door in a scene that if i press E opens and doesnt stop untill animation finish and when i press E again closes , simple animation for test

---
+++ Assets/Code/Scripts/Dimitris/Door.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+/// <summary>
+/// Door class that implements an interaction interface , basically open and close door
+/// </summary>
+public class Door : MonoBehaviour,IInteractable<MonoBehaviour>
+{
+    [SerializeField] private Animation anim;
+    [SerializeField] private string doorAnimationName = "Door Open";
+
+    private bool isOpen = false;// Checks if door is currently open
+    private bool isAnimating = false; // Prevents interaction while animation is playing
+
+    public bool CanInteract(MonoBehaviour Interactor)
+    {
+        return !isAnimating;  // Interaction is only allowed if not animating
+    }
+    // Attempts to interact with the door
+    public bool TryInteract(MonoBehaviour Interactor) 
+    {
+        if(!CanInteract(Interactor))
+        {
+            return false; 
+        }
+        PlayAnimation();//Animation PLay
+        return true;
+    }
+    // Handles playing the animation forward or backward
+    private void PlayAnimation()
+    {
+        AnimationState state = anim[doorAnimationName];
+        if (!isOpen)
+        {
+            state.speed = 1f;
+            state.time = 0f;
+        }
+        else
+        {
+            state.speed = -1f;
+            state.time = state.length;
+        }
+        anim.Play(doorAnimationName);
+        isAnimating = true;
+        StartCoroutine(WaitForAnimation(state.length));
+        isOpen = !isOpen;//Door State change (Open or Closed = !isOpen)
+    }
+    // Coroutine that waits for the animation to finish
+    private System.Collections.IEnumerator WaitForAnimation(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        isAnimating = false;
+    }
+}
