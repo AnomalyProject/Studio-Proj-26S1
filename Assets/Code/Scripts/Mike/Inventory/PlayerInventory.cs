dd3ba31b3be0d2570695b3e88ca0bc1704085a8b
mikepaneras <mikepaneras21@gmail.com>
2026-04-06 22:04:47 +0300

    -Applied player inventory to prefabs

--- Assets/Code/Scripts/Mike/Inventory/PlayerInventory.cs
+++ Assets/Code/Scripts/Mike/Inventory/PlayerInventory.cs
@@ -69,36 +69,32 @@ public class PlayerInventory : MonoBehaviour
     #region Inventory Control
     public void NextItem()
     {
-        int current = focusedIndex + 1;
+        int current = focusedIndex;
 
-        while(current != focusedIndex)
+        for (int i = 0; i < Inventory.TotalSlots; i++)
         {
-            if(current >= Inventory.TotalSlots) current = 0;
-
-            if(Inventory.TryGet(current, out IReadOnlyItemStack stack))
-            {
-                ChangeFocused(current);
-                break;
-            }
-
-            current++;
-        }
-    }
-    public void PreviousItem()
-    {
-        int current = focusedIndex - 1;
-
-        while (current != focusedIndex)
-        {
-            if (current < 0) current = Inventory.TotalSlots - 1;
+            current = (current + 1) % Inventory.TotalSlots;
 
             if (Inventory.TryGet(current, out IReadOnlyItemStack stack))
             {
                 ChangeFocused(current);
-                break;
+                return;
             }
+        }
+    }
+    public void PreviousItem()
+    {
+        int current = focusedIndex;
 
-            current--;
+        for (int i = 0; i < Inventory.TotalSlots; i++)
+        {
+            current = (current - 1 + Inventory.TotalSlots) % Inventory.TotalSlots;
+
+            if (Inventory.TryGet(current, out IReadOnlyItemStack stack))
+            {
+                ChangeFocused(current);
+                return;
+            }
         }
     }
     public void ChangeFocused(int focusAtIndex)
