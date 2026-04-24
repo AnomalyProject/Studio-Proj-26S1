977106c745e17db9567b6825816a4161ad468c45
shikpurple <mi.angelakou@hotmail.com>
2026-04-24 23:19:41 +0300

    Fixed Event Volume and caught up with 306 COMMITS FROM DEVELOP JFC

--- Assets/Code/Scripts/Vasilis/EventVolume.cs
+++ Assets/Code/Scripts/Vasilis/EventVolume.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
 using UnityEngine.Events;
-
+using PurrNet;
 
 [RequireComponent(typeof(Collider))]
-public class EventVolume : MonoBehaviour
+public class EventVolume : NetworkBehaviour
+
 {
     [Header("Detection Settings")]
     [Tooltip("Only objects on these layers will fire the events.")]
@@ -22,23 +23,31 @@ public class EventVolume : MonoBehaviour
         col.isTrigger = true;
     }
 
+    [ObserversRpc] private void InvokeTrigger_ObserversRPC(bool enter)
+    {
+        if (enter) TriggerEntered?.Invoke();
+        else TriggerExited?.Invoke();
+    }
     private void OnTriggerEnter(Collider other)
     {
-     
+
+        if (!isServer) return;
+
         if (((1 << other.gameObject.layer) & detectionLayers) != 0)
         {
             Debug.Log($"{other.name} entered {gameObject.name}");
-            TriggerEntered?.Invoke();
+            InvokeTrigger_ObserversRPC(true);
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-      
+        if (!isServer) return;
+
         if (((1 << other.gameObject.layer) & detectionLayers) != 0)
         {
             Debug.Log($"{other.name} exited {gameObject.name}");
-            TriggerExited?.Invoke();
+            InvokeTrigger_ObserversRPC(false);
         }
     }
 }
