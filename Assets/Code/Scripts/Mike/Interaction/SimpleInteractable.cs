55447889e7550d69657314b5661177f5a18390c8
mikepaneras <mikepaneras21@gmail.com>
2026-04-30 05:25:12 +0300

    -changed example inteactable to simple interactable

---
+++ Assets/Code/Scripts/Mike/Interaction/SimpleInteractable.cs
@@ -0,0 +1,18 @@
+using PurrNet;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SimpleInteractable : NetworkBehaviour, IInteractable<MonoBehaviour>
+{
+    private UnityEvent OnInteracted;
+    public Task<bool> CanInteract(MonoBehaviour interactor) => Task.FromResult(true);
+
+    [ServerRpc] public Task<bool> TryInteract(MonoBehaviour interactor)
+    {
+        OnInteracted?.Invoke();
+        return Task.FromResult(true);
+    }
+
+    [ObserversRpc] private void InvokeInteracted() => OnInteracted?.Invoke();
+}
