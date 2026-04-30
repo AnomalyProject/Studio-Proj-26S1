eb9432179a6b38c89fe7cfc7f7c056d005937e41
GeoD <geo2006@hotmail.gr>
2026-04-30 21:18:00 +0300

    Enemy AI New features
    Added:
    Player Observe thing (Mock)
    Shift Patrol Point Priority (Mock)
    Hearing System
    2 New States

--- Assets/Code/Scripts/GiorgosD/Enemy AI/EnemyBrain.cs
+++ Assets/Code/Scripts/GiorgosD/Enemy AI/EnemyBrain.cs
@@ -2,10 +2,11 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBrain : MonoBehaviour
+public class EnemyBrain : MonoBehaviour, IAlertable
 {
     [Header("Patrol Settings")]
     [SerializeField] private List<Transform> patrolPoints = new List<Transform>();
+    [SerializeField] private List<Transform> patrolPriorities = new List<Transform>();
     [SerializeField, Tooltip("How long the AI will stay idle before it continues to other stuff")] private float idleTimer;
 
     [Header("Chase Settings")]
@@ -16,13 +17,24 @@ public class EnemyBrain : MonoBehaviour
     private EnemyPawn body;
     private BaseState currentState;
 
+    // TEMP stuff bc no events fire at the moment
+    public bool poiIsEnabled;
+    private AI_TestHelper testHelper;
+
     public List<Transform> PatrolPoints => patrolPoints;
+    public List<Transform> PatrolPriorities => patrolPriorities;
     public float IdleTime => idleTimer;
     public List<Transform> RespawnPoints => respawnPoints;
 
     private void Start()
     {
         body = GetComponent<EnemyPawn>();
+
+        testHelper = FindFirstObjectByType<AI_TestHelper>().GetComponent<AI_TestHelper>();
+
+        testHelper.onWatched.AddListener(OnObservedTooMuch);
+
+        testHelper.onItemPicked.AddListener(OnItemPicked);
 
         ChangeState(new PatrolState(this, body));
     }
@@ -39,5 +51,31 @@ public class EnemyBrain : MonoBehaviour
     private void Update()
     {
         currentState?.Update();
+    }
+
+    /// <summary>
+    /// Gets called when the player has been looking at the enemy for too long.
+    /// </summary>
+    private void OnObservedTooMuch(Transform player)
+    {
+        ChangeState(new AlertState(this, body, player));
+    }
+
+    private void OnItemPicked()
+    {
+        poiIsEnabled = !poiIsEnabled;
+    }
+
+    public void Alert<TTarget>(TTarget alertedBy) where TTarget : MonoBehaviour
+    {
+        if (currentState is ChaseState || currentState is AttackState || currentState is AlertState) return;
+
+        ChangeState(new AlertState(this, body, alertedBy.transform));
+    }
+
+    private void OnDestroy()
+    {
+        testHelper.onItemPicked.RemoveListener(OnItemPicked);
+        testHelper.onWatched.RemoveListener(OnObservedTooMuch);
     }
 }
