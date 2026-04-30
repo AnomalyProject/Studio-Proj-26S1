27d6f4723495617e1b8f8b53a3bb239703701fb7
mikepaneras <mikepaneras21@gmail.com>
2026-04-30 05:25:28 +0300

    Update SimpleInteractable.cs

--- Assets/Code/Scripts/Mike/Interaction/SimpleInteractable.cs
+++ Assets/Code/Scripts/Mike/Interaction/SimpleInteractable.cs
@@ -5,7 +5,7 @@ using UnityEngine.Events;
 
 public class SimpleInteractable : NetworkBehaviour, IInteractable<MonoBehaviour>
 {
-    private UnityEvent OnInteracted;
+    [SerializeField] private UnityEvent OnInteracted;
     public Task<bool> CanInteract(MonoBehaviour interactor) => Task.FromResult(true);
 
     [ServerRpc] public Task<bool> TryInteract(MonoBehaviour interactor)
