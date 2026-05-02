f92e0a9269d908b1e96a91c4053cc1c94518b1a9
mikepaneras <mikepaneras21@gmail.com>
2026-05-02 08:14:24 +0300

    -Elevator Sounds & Game State Sounds

--- Assets/Code/Scripts/Mike/Audio Scripts/GameSounds.cs
+++ Assets/Code/Scripts/Mike/Audio Scripts/GameSounds.cs
@@ -2,13 +2,27 @@ using UnityEngine;
 
 public class GameSounds : SoundCaller
 {
-    [SerializeField] AudioClip voidTimerTick, voidTimerOver;
+    [SerializeField] AnomalyManager anomalyManager;
+    [SerializeField] AudioClip voidTimerTick, voidTimerOver, enteredVoidClip, winGameClip;
     [SerializeField, Min(1)] int warningTicksAtSeconds;
-
     public void OnVoidTimerOver() => PlaySFXClip(voidTimerOver);
     public void OnVoidTimerTick(float currentTime)
     {
         if(currentTime <= warningTicksAtSeconds && currentTime != 0)
         PlaySFXClip(voidTimerTick);
+    }
+
+    public void OnElevatorOpened()
+    {
+        switch (anomalyManager.CurrentState)
+        {
+            case AnomalyManager.RoomState.PunishmentRoom:
+                PlaySFXClip(enteredVoidClip);
+                break;
+
+            case AnomalyManager.RoomState.WinRoom:
+                PlaySFXClip(winGameClip);
+                break;
+        }
     }
 }
