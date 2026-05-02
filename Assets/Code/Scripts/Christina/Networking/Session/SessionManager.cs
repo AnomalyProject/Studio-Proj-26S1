13aaa03c9e5522865863b7dcb53e879fba21957b
Christina Kaliora <christina.kaliora@hotmail.com>
2026-05-02 21:45:36 +0300

    Update SessionManager.cs

--- Assets/Code/Scripts/Christina/Networking/Session/SessionManager.cs
+++ Assets/Code/Scripts/Christina/Networking/Session/SessionManager.cs
@@ -567,6 +567,7 @@ public class SessionManager : NetworkBehaviour, IPlayerEvents
             return false;
         }
 
+        // todelete: dummy comment to push and get a new build
         SessionModeManager.Instance.LoadGameplayScene();
         Debug.Log("[SessionManager] Elevator locked. Game starting...");
         return true;
