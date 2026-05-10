52343d84425518661190850499094aba0182bc9f
Konstantinos Mathioudakis <agreekdev@gmail.com>
2026-05-10 04:32:10 +0300

    Settings Menu
    Scene: Assets/Scenes/Konstantinos/KonstantinosMainMenu.unity
    
    Prefab: Assets/Prefabs/Managers/SettingsManager.prefab
    
    Script: Assets/Code/Scripts/Konstantinos/UI/SettingsManager.cs
    
    Also made a font variant at: Assets/Art/Fonts/digital-7 SDF (OUTLINE).asset
    
    Settings include:
    - All requested options fields
    - Working logic
    - Local saving using PlayerPrefs
    - Also included a settings category filtering menu with a custom selection outline
    
    What's missing (To Do):
    - Audio Manager does not change audio volume
    - Hook options from the "Controls" category to the player
    - (possibly) more settings options
    - Proper Keyboard/Controller navigation

--- Assets/Code/Scripts/Konstantinos/UI/SettingsManager.cs
+++ Assets/Code/Scripts/Konstantinos/UI/SettingsManager.cs
@@ -3,22 +3,23 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using TMPro;
 using UnityEngine.UI;
-using System.Collections;
+using System.Collections.Generic;
 
 public class SettingsManager : MonoBehaviour
 {
+    // Singleton reference
     public static SettingsManager Instance { get; private set; }
 
-    private bool Initialized;
+    private bool Initialized; // for running pieces of code ONLY after everything is initialized 
 
     // Events
     public static event Action OnSettingsOpened;
     public static event Action OnSettingsClosed;
 
     [Header("Categories")]
-    [SerializeField] RectTransform CategoryHolderTransform;
+    [SerializeField] RectTransform CategoryHolderTransform; // holds all category tabs
     [SerializeField] Vector2[] CatHolderSizeDeltas; // resize holder whenever category changes
-    [SerializeField] GameObject[] Categories, CategoryButtonsOutline;
+    [SerializeField] GameObject[] Categories, CategoryButtonsOutline; // Category tabs & filter button outlines
     [SerializeField] int selectedCategory = -1; // -1 = all categories. Then specific category index goes like 0, 1, 2 etc
     private float[] categoryYPositions;
 
@@ -27,8 +28,37 @@ public class SettingsManager : MonoBehaviour
     [SerializeField] GameObject firstSelectedElement;
 
     [Header("Settings Controls References")]
-    [SerializeField] private Slider masterVolumeSlider;
-    [SerializeField] private TMP_Dropdown resolutionDropdown;
+    // Audio
+    [SerializeField] Slider masterVolumeSlider;
+    [SerializeField] Slider musicVolumeSlider;
+    [SerializeField] Slider SFXVolumeSlider;
+    [SerializeField] Slider UIVolumeSlider;
+
+    // Graphics
+    [SerializeField] TMP_Dropdown resolutionDropdown;
+    [SerializeField] Toggle vSyncToggle;
+    [SerializeField] Toggle fullscreenToggle;
+
+    // Controls
+    [SerializeField] Slider lookSensitivitySlider;
+    [SerializeField] Toggle invertLookXToggle, invertLookYToggle;
+
+    //------------------------------------------------------------//
+
+    // Saveable variables: 
+
+    // Audio
+    private float masterVolume = 1.0f, musicVolume = 1.0f, SFXVolume = 1.0f, UIVolume = 1.0f;
+
+    // Graphics 
+    private int resolutionValue;
+    private bool vSync, fullscreen;
+
+    // Controls
+    private float lookSensitivity = 0.7f;
+    private bool invertLookX, invertLookY;
+
+    //------------------------------------------------------------//
 
     private void Awake()
     {
@@ -40,23 +70,25 @@ public class SettingsManager : MonoBehaviour
         }
         Instance = this;
 
-        DontDestroyOnLoad(gameObject);
+        DontDestroyOnLoad(gameObject);      
+    }
 
-        //initialization
+    private void Start()
+    {
+        // initialization happens on Start() because it depends on other Systems to be singleton first like the AudioManager
         Close();
         InitializeSettings();
     }
 
-
+    #region Initializations
     public void InitializeSettings()
     {
-        // Makes sure the slider/dropdown/toggle values are correct
-
-        //masterVolumeSlider.value = AudioManager.Instance.
-        //resolutionDropdown.value = 
-
-
         InitializeCategories();
+        InitializeAudio();
+        InitializeGraphics();
+        InitializeControls();
+        InitializeAccessibility();
+
         Initialized = true;
     }
 
@@ -65,26 +97,127 @@ public class SettingsManager : MonoBehaviour
         categoryYPositions = new float[Categories.Length];
         for (int i = 0; i < Categories.Length; i++)
         {
-            categoryYPositions[i] = Categories[i].transform.localPosition.y;
+            categoryYPositions[i] = Categories[i].transform.localPosition.y; // store y position for later repositioning
         }
 
         SelectCategory(-1); // enable all
     }
 
+    void InitializeAudio()
+    {
+        // Get any saved data
+        if (PlayerPrefs.HasKey("Master Volume")) masterVolume = PlayerPrefs.GetFloat("Master Volume");
+        if (PlayerPrefs.HasKey("Music Volume")) musicVolume = PlayerPrefs.GetFloat("Music Volume");
+        if (PlayerPrefs.HasKey("SFX Volume")) SFXVolume = PlayerPrefs.GetFloat("SFX Volume");
+        if (PlayerPrefs.HasKey("UI Volume")) UIVolume = PlayerPrefs.GetFloat("UI Volume");
+
+        // Apply the values on the Sliders (OnValueChanged gets called)
+        masterVolumeSlider.value = masterVolume;
+        musicVolumeSlider.value = musicVolume;
+        SFXVolumeSlider.value = SFXVolume;
+        UIVolumeSlider.value = UIVolume;
+    }
+
+    void InitializeGraphics()
+    {
+        // Initialize Resolutions
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        // Get all supported resolutions
+        for (int i = 0; i < Screen.resolutions.Length; i++)
+        {
+            string option =
+                $"{Screen.resolutions[i].width} x {Screen.resolutions[i].height} @ {Screen.resolutions[i].refreshRateRatio.value:F0}Hz";
+
+            options.Add(option);
+
+            // Match current screen resolution
+            if (Screen.resolutions[i].width == Screen.currentResolution.width &&
+                Screen.resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        // add all supported resolutions as options on the dropdown
+        resolutionDropdown.AddOptions(options);
+
+        if (PlayerPrefs.HasKey("Resolution"))
+            currentResolutionIndex = PlayerPrefs.GetInt("Resolution"); // apply the last resolution if previously changed
+
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+        resolutionValue = currentResolutionIndex;
+
+
+
+        // Initialize V-Sync
+        if (PlayerPrefs.HasKey("V-Sync")) vSync = bool.Parse(PlayerPrefs.GetString("V-Sync"));
+        else vSync = true;
+
+        vSyncToggle.isOn = vSync;
+
+
+
+        // Initialize FullScreen
+        if (PlayerPrefs.HasKey("Fullscreen")) fullscreen = bool.Parse(PlayerPrefs.GetString("Fullscreen"));
+        else fullscreen = true; 
+
+        fullscreenToggle.isOn = fullscreen;
+    }
+
+    void InitializeControls()
+    {
+        // Initialize Look Sensitivity
+        if (PlayerPrefs.HasKey("Look Sensitivity")) lookSensitivity = PlayerPrefs.GetFloat("Look Sensitivity");
+        
+        lookSensitivitySlider.value = lookSensitivity;
+
+
+
+        // Initialize Invert Look
+        if (PlayerPrefs.HasKey("Invert Look X")) invertLookX = bool.Parse(PlayerPrefs.GetString("Invert Look X"));
+        else invertLookX = false;
+
+        invertLookXToggle.isOn = invertLookX;
+
+
+        if (PlayerPrefs.HasKey("Invert Look Y")) invertLookY = bool.Parse(PlayerPrefs.GetString("Invert Look Y"));
+        else invertLookY = false;
+        
+        invertLookYToggle.isOn = invertLookY;
+    }
+
+    void InitializeAccessibility()
+    {
+        // WIP
+    }
+    #endregion
+
+    // Static method, opens canvas, SettingsManager.Open()
     public static void Open()
     {
         Instance?.settingsCanvas.SetActive(true);
-        Instance?.InitializeSettings();
         IsOpen = true;
         EventSystem.current.SetSelectedGameObject(Instance.firstSelectedElement);
         OnSettingsOpened?.Invoke();
 
         if (Instance.Initialized)
         {
-            Instance?.SelectCategory(-1);
+            Instance?.SelectCategory(-1); // selects all
+        }
+        else
+        {
+            Instance?.InitializeSettings();
         }
     }
 
+    // Static method, closes canvas, SettingsManager.Close()
     public static void Close()
     {
         Instance?.settingsCanvas.SetActive(false);
@@ -100,12 +233,14 @@ public class SettingsManager : MonoBehaviour
 
     public static bool IsOpen { get; private set; }
 
+    // filters and shows a specific category
     public void SelectCategory(int index)
     {
         selectedCategory = index;
 
         if (CatHolderSizeDeltas.Length > 0)
         {
+            // resize holder so there's no dead scroll space for each category
             CategoryHolderTransform.sizeDelta = CatHolderSizeDeltas[selectedCategory + 1];
         }
 
@@ -114,16 +249,20 @@ public class SettingsManager : MonoBehaviour
             for (int i = 0; i < Categories.Length; i++)
             {
                 Categories[i]?.SetActive(true);
+
+                // each category moves to their original positions
                 Categories[i].transform.localPosition = new Vector3(Categories[i].transform.localPosition.x, categoryYPositions[i], 0);
             }
         }
-        else
+        else // specific category
         {
             foreach (GameObject cat in Categories)
             {
                 cat?.SetActive(false);
             }
             Categories[selectedCategory]?.SetActive(true);
+
+            // move our category to the top
             Categories[selectedCategory].transform.localPosition = new Vector3(Categories[selectedCategory].transform.localPosition.x, categoryYPositions[0], 0);
         }
 
@@ -135,14 +274,137 @@ public class SettingsManager : MonoBehaviour
         CategoryButtonsOutline[selectedCategory + 1]?.SetActive(true);
     }
 
+    //------------------------------------------------------------//
+
+    #region Options Change Methods
+
+    // Audio
     public void OnMasterVolumeChanged(float value)
     {
-        AudioManager.Instance.Volume(AudioManager.AudioChannel.Master, value);
+        masterVolume = value;
+
+        // Convert linear slider position into logarithmic response
+        float logarithmicValue = Mathf.Log10(Mathf.Lerp(0.0001f, 1f, masterVolume)) + 1f;
+
+        AudioManager.Instance.Volume(AudioManager.AudioChannel.Master, logarithmicValue);
+
+        PlayerPrefs.SetFloat("Master Volume", masterVolume); // save linear value so we can call the method with it
     }
 
+    public void OnMusicVolumeChanged(float value)
+    {
+        musicVolume = value;
+
+        // Convert linear slider position into logarithmic response
+        float logarithmicValue = Mathf.Log10(Mathf.Lerp(0.0001f, 1f, value)) + 1f;
+
+        AudioManager.Instance.Volume(AudioManager.AudioChannel.Music, logarithmicValue);
+
+        PlayerPrefs.SetFloat("Music Volume", musicVolume); // save linear value so we can call the method with it
+    }
+
+    public void OnSFXVolumeChanged(float value)
+    {
+        SFXVolume = value;
+
+        // Convert linear slider position into logarithmic response
+        float logarithmicValue = Mathf.Log10(Mathf.Lerp(0.0001f, 1f, value)) + 1f;
+
+        AudioManager.Instance.Volume(AudioManager.AudioChannel.SFX, logarithmicValue);
+
+        PlayerPrefs.SetFloat("SFX Volume", SFXVolume); // save linear value so we can call the method with it
+    }
+
+    public void OnUIVolumeChanged(float value)
+    {
+        UIVolume = value;
+
+        // Convert linear slider position into logarithmic response
+        float logarithmicValue = Mathf.Log10(Mathf.Lerp(0.0001f, 1f, value)) + 1f;
+
+        AudioManager.Instance.Volume(AudioManager.AudioChannel.UI, logarithmicValue);
+
+        PlayerPrefs.SetFloat("UI Volume", UIVolume); // save linear value so we can call the method with it
+    }
+
+    //------------------------------------------------------------//
+    // Graphics
     public void OnResolutionChanged(int value)
     {
-        Debug.Log($"[from: SettingsManager] Resolution has changed to {value}");
-        // TODO: Hook into resolution/display system when available
+        resolutionValue = value;
+
+        Resolution selectedResolution = Screen.resolutions[value];
+
+        Debug.Log(
+            $"[SettingsManager] Resolution changed to " +
+            $"{selectedResolution.width}x{selectedResolution.height}"
+        );
+
+        Screen.SetResolution(
+          selectedResolution.width,
+          selectedResolution.height,
+          Screen.fullScreenMode,
+          selectedResolution.refreshRateRatio
+        );
+
+        PlayerPrefs.SetInt("Resolution", resolutionValue);
     }
+
+    public void ToggleVSync(bool value)
+    {
+        vSync = value;
+
+        Debug.Log($"[SettingsManager] V-Sync changed to {vSync}");
+
+        if (value) QualitySettings.vSyncCount = 1;
+        else QualitySettings.vSyncCount = 0;
+
+        PlayerPrefs.SetString("V-Sync", vSync.ToString());
+    }
+
+    public void ToggleFullscreen(bool value)
+    {
+        fullscreen = value;
+
+        Debug.Log($"[SettingsManager] Fullscreen changed to {fullscreen}");
+
+        Screen.fullScreen = fullscreen;
+
+        PlayerPrefs.SetString("Fullscreen", vSync.ToString());
+    }
+
+    //------------------------------------------------------------//
+    // Controls
+    public void OnLookSensitivityChanged(float value)
+    {
+        lookSensitivity = value;
+
+        Debug.Log($"[SettingsManager] Look Sensitivity changed to {lookSensitivity}");
+        //TODO: Hook this to the player
+
+        PlayerPrefs.SetFloat("Look Sensitivity", lookSensitivity);
+    }
+
+    public void ToggleInvertXLook(bool value)
+    {
+        invertLookX = value;
+
+        Debug.Log($"[SettingsManager] Invert Look X changed to {invertLookX}");
+
+        PlayerPrefs.SetString("Invert Look X", invertLookX.ToString());
+
+        //TODO: Hook this to the player
+    }
+
+    public void ToggleInvertYLook(bool value)
+    {
+        invertLookY = value;
+
+        Debug.Log($"[SettingsManager] Invert Look Y changed to {invertLookY}");
+
+        PlayerPrefs.SetString("Invert Look Y", invertLookY.ToString());
+
+        //TODO: Hook this to the player
+    }
+    #endregion
 }
