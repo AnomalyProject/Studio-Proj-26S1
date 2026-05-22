dea8029670430166932756445ed8bf80d39b1c41
NestorasAngelopoulos <nestorasangelopoulos@gmail.com>
2026-05-23 00:32:54 +0300

    Integrated gameplay elements into each area

--- Assets/Code/Scripts/Nestoras/LightSwitcher.cs
+++ Assets/Code/Scripts/Nestoras/LightSwitcher.cs
@@ -1,20 +1,40 @@
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
 
+[CustomEditor(typeof(LightSwitcher))]
+public class LightSwitcherInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("Next")) ((LightSwitcher)target).Next();
+    }
+}
+#endif
+
+[ExecuteAlways]
 public class LightSwitcher : MonoBehaviour
 {
     [SerializeField] private Light[] lights;
     [SerializeField] private int index = 0;
 
-    private void Awake() => lights[index].enabled = true;
+    private void Awake()
+    {
+        foreach (Light light in lights) light.gameObject.SetActive(false);
+        lights[index].gameObject.SetActive(true);
+    }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Home))
-        {
-            lights[index++].enabled = false;
-            if (index >= lights.Length) index = 0;
-            lights[index].enabled = true;
-            Debug.Log($"Set Directional Light to {lights[index].name}");
-        }
+        if (Input.GetKeyDown(KeyCode.Home)) Next();
+    }
+
+    public void Next()
+    {
+        lights[index++].gameObject.SetActive(false);
+        if (index >= lights.Length) index = 0;
+        lights[index].gameObject.SetActive(true);
+        Debug.Log($"Set Directional Light to {lights[index].name}");
     }
 }
