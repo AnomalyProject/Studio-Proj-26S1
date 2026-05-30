fca2eeb47c3dbe632b25ed4a7c4642b4645060cb
dataktos <dataktos@gmail.com>
2026-05-30 03:01:55 +0300

    refactor done
    lean final

--- Assets/Code/Scripts/Dimitris/PlayerLeanAnimation.cs
+++ Assets/Code/Scripts/Dimitris/PlayerLeanAnimation.cs
@@ -7,38 +7,49 @@ public class PlayerLeanAnimation : NetworkBehaviour
     [Header("References")]
     [SerializeField] private Transform affectedBone;// Bone to rotate
     [SerializeField] private CameraLean cameraLean;
+    [SerializeField] private FPSCameraController cameraController;
 
     [Header("Settings")]
     [SerializeField] private float maxAngle = 15f;
     [SerializeField] private float lerpSpeed = 8f;
+    [Header("Vertical Settings")]
+    [SerializeField] private float maxVerticalAngle = 35f;
+    [SerializeField] private float lerpVerticalSpeed = 8f;
 
     private float currentLean;
     private SyncVar<int> effectedLean = new SyncVar<int>(0 , ownerAuth: true) ;
 
+    private float currentPitchVisual;
+    private SyncVar<float> syncedPitch = new SyncVar<float>(0f, ownerAuth: true);
+
     private void LateUpdate()
     { // LateUpdate so not be confused with animator
 
-
-
         if(isOwner)
-            {
+        {
             if (cameraLean != null)
             {
                 effectedLean.value = (int)Mathf.Clamp(cameraLean.LeanAmount, -1f, 1f);
             }
-            return;
+            if (cameraController != null)
+            {
+                syncedPitch.value = Mathf.Clamp(cameraController.CurrentPitch / maxVerticalAngle, -1f, 1f);
+            }
+                return;
         }
-            forceLean(effectedLean.value);
+            forceLean(effectedLean.value, syncedPitch.value);
         
     }
 
-   private void forceLean (float targetLean)
+   private void forceLean (float targetLean , float targetPitch)
     {
        // Debug.Log($"Target Lean: {targetLean} | Current Lean: {currentLean}");
         currentLean = Mathf.Lerp(currentLean, targetLean, Time.deltaTime * lerpSpeed);
+        currentPitchVisual = Mathf.Lerp(currentPitchVisual, targetPitch, Time.deltaTime * lerpVerticalSpeed);
         float angle = currentLean * maxAngle;
+        float pitchAngle = currentPitchVisual * maxVerticalAngle;
         Vector3 currentEuler = affectedBone.localEulerAngles;
         //Rotation to Z
-        affectedBone.localRotation = Quaternion.Euler(currentEuler.x, currentEuler.y, angle);
+        affectedBone.localRotation = Quaternion.Euler(pitchAngle, currentEuler.y, angle);
     }
 }
