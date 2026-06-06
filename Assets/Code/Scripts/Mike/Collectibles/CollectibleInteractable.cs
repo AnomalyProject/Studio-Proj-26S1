920f0750b31f884f5e4e5902fa5323c818ebb0f6
mikepaneras <mikepaneras21@gmail.com>
2026-06-06 23:07:34 +0300

    -an extra event

--- Assets/Code/Scripts/Mike/Collectibles/CollectibleInteractable.cs
+++ Assets/Code/Scripts/Mike/Collectibles/CollectibleInteractable.cs
@@ -6,19 +6,22 @@ public class CollectibleInteractable : MonoBehaviour, IInteractable<PlayerBody>
 {
     [SerializeField] private CollectibleSO collectibleData;
     [SerializeField] private UnityEvent onCollected;
+    [SerializeField, Tooltip("Called in awake if already gathered or after succesfully collecting via interaction.")] private UnityEvent onDisableEffects;
 
     public CollectibleSO CollectibleData => collectibleData;
+    public bool CollectibleGathered => RefrenceManager.CurrentSave.collectiblesGathered.Contains(collectibleData.ID);
 
-    public Task<bool> CanInteract(PlayerBody interactor)
+    private void Awake()
     {
-        bool hasCollectible = RefrenceManager.CurrentSave.collectiblesGathered.Contains(collectibleData.ID);
-        return Task.FromResult(!hasCollectible);
+        if (CollectibleGathered) onDisableEffects?.Invoke();
     }
 
+    public Task<bool> CanInteract(PlayerBody interactor) => Task.FromResult(!CollectibleGathered);
     public async Task<bool> TryInteract(PlayerBody interactor)
     {
         RefrenceManager.CurrentSave.collectiblesGathered.Add(collectibleData.ID);
         onCollected.Invoke();
+        onDisableEffects?.Invoke();
         await Task.Run(() => SaveSystem.QuickSave(RefrenceManager.CurrentSave));
         return true;
     }
