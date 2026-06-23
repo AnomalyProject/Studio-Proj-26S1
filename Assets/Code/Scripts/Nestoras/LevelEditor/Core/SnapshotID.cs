0debfa1ded4ffc7532871360302429e109da06e2
Nestoras Angelopoulos <nestorasangelopoulos@gmail.com>
2026-06-23 15:00:59 +0300

    Sped up Level Editor loading times by 9000000%

--- Assets/Code/Scripts/Nestoras/LevelEditor/Core/SnapshotID.cs
+++ Assets/Code/Scripts/Nestoras/LevelEditor/Core/SnapshotID.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 #if UNITY_EDITOR
 using UnityEditor;
-using System.Linq;
 
 [CustomEditor(typeof(SnapshotID))]
 public class SnapshotIDEditor : Editor
@@ -16,16 +18,25 @@ public class SnapshotIDEditor : Editor
 /// 
 /// A GUID wrapper for <see cref="GameObject"/>s that need to be tracked in <see cref="LevelModification"/>s.
 /// </summary>
-[ExecuteAlways]
+//[ExecuteAlways]
 [AddComponentMenu("")]
 public class SnapshotID : MonoBehaviour
 {
     public string guid;
     private void Awake() => EnsureGuid();
 
+    private static Dictionary<string, SnapshotID> snapshotIDs = new Dictionary<string, SnapshotID>();
+
 #if UNITY_EDITOR
     // Runs in editor when object is created, duplicated, or modified
     private void OnValidate() => EnsureGuid();
+
+    private bool listMarkedForDeletion = false;
+    private void ResetGUIDsNextFrame()
+    {
+        snapshotIDs.Clear();
+        listMarkedForDeletion = false;
+    }
 #endif
 
     private void EnsureGuid()
@@ -34,6 +45,7 @@ public class SnapshotID : MonoBehaviour
         if (string.IsNullOrEmpty(guid))
         {
             guid = System.Guid.NewGuid().ToString();
+            snapshotIDs.Add(guid, this);
             return;
         }
 
@@ -42,13 +54,34 @@ public class SnapshotID : MonoBehaviour
         if (EditorApplication.isPlayingOrWillChangePlaymode) return; // Don't regenerate GUID when entering playmode.
 
         // Detect duplicates in the scene (Ctrl+D)
-        SnapshotID[] all = FindObjectsByType<SnapshotID>(FindObjectsInactive.Include, FindObjectsSortMode.None);
-        bool duplicateExists = all.Any(x => x != this && x.guid == guid);
-        if (duplicateExists)
+        if (snapshotIDs.Count == 0)
         {
-            guid = System.Guid.NewGuid().ToString();
-            EditorUtility.SetDirty(this); // Mark dirty so Unity saves the change
+            if (!listMarkedForDeletion) EditorApplication.delayCall += ResetGUIDsNextFrame;
+            listMarkedForDeletion = true;
+            try
+            {
+                snapshotIDs.AddRange(FindObjectsByType<SnapshotID>(FindObjectsInactive.Include, FindObjectsSortMode.None).ToDictionary(x => x.guid));
+            }
+            catch // Duplicates Occured
+            {
+                guid = System.Guid.NewGuid().ToString();
+                EditorUtility.SetDirty(this); // Mark dirty so Unity saves the change
+                snapshotIDs.Clear(); // Scan again in next object
+                return;
+            }
         }
+
+        try // snapshotIDs could be uninitialized
+        {
+            bool duplicateExists = snapshotIDs[guid] != this;
+            if (duplicateExists)
+            {
+                guid = System.Guid.NewGuid().ToString();
+                snapshotIDs.Add(guid, this);
+                EditorUtility.SetDirty(this); // Mark dirty so Unity saves the change
+            }
+        }
+        catch { }
 #endif
     }
 }
